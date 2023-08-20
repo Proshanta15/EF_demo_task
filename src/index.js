c42388fd8044e67084764e69353cf33704a65ff4
proshantakroy <codemen@pro>
2023-08-20 23:07:17 +0600

    Deploy EF task

---
+++ src/index.js
@@ -0,0 +1,66 @@
+
+import logoImg from './assets/EF_logo.png';
+import header_img from './assets/bg.jpg';
+import image_2 from './assets/horse.jpeg';
+import image_3 from './assets/remnant.jpg';
+import image_1 from './assets/sengoku.jpeg';
+
+import menuImage_1 from './assets/steam.png';
+import menuImage_2 from './assets/ubisoft_icon.png';
+import menuImage_3 from './assets/ea.png';
+import menuImage_4 from './assets/battle-net.png';
+import menuImage_5 from './assets/rockstar.png';
+import menuImage_6 from './assets/gog.png';
+import menuImage_7 from './assets/microsoft.png';
+import menuImage_8 from './assets/epic.png';
+
+import menuImage_11 from './assets/steam_two.png';
+
+import menuImage_12 from './assets/IG.png';
+
+import menuImage_13 from './assets/blizzard.png';
+import './styles/main.scss';
+
+/****** Image******/
+const menuImg_1 = document.getElementById('menu_two_1')
+menuImg_1.src = menuImage_1;
+const menuImg_2 = document.getElementById('menu_two_2')
+menuImg_2.src = menuImage_2;
+const menuImg_3 = document.getElementById('menu_two_3')
+menuImg_3.src = menuImage_3;
+const menuImg_4 = document.getElementById('menu_two_4')
+menuImg_4.src = menuImage_4;
+const menuImg_5 = document.getElementById('menu_two_5')
+menuImg_5.src = menuImage_5;
+const menuImg_6 = document.getElementById('menu_two_6')
+menuImg_6.src = menuImage_6;
+const menuImg_7 = document.getElementById('menu_two_7')
+menuImg_7.src = menuImage_7;
+const menuImg_8 = document.getElementById('menu_two_8')
+menuImg_8.src = menuImage_8;
+
+const menuImg_11 = document.getElementById('menu_two_11')
+menuImg_11.src = menuImage_11;
+const menuImg_12 = document.getElementById('menu_two_12')
+menuImg_12.src = menuImage_12;
+
+const menuImg_13 = document.getElementById('menu_two_13')
+menuImg_13.src = menuImage_13;
+/****menu_two****** */
+const logoImage = document.getElementById('logo')
+logoImage.src = logoImg;
+
+const headerImg = document.getElementById('header_skew')
+headerImg.src = header_img;
+
+const Img1 = document.getElementById('image1')
+Img1.src = image_1;
+
+const Img2 = document.getElementById('image2')
+Img2.src = image_2;
+
+const Img3 = document.getElementById('image3')
+Img3.src = image_3;
+/****** Image******/
+
+
